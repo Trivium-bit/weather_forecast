28ba39025527ab2374d77ace5d8fe77066f1e397
Trivium-bit <trivium.exe@gmail.com>
2021-10-29 08:37:01 +0300

    app

--- src/App.js
+++ src/App.js
@@ -15,20 +15,30 @@ class WeatherDisplay extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0,
+    };
+  }
   render() {
+    const activePlace = this.state.activePlace;
     return (
       <div className="App">
         {PLACES.map((place, index) => (
           <button
             key={index}
             onClick={() => {
-              console.log("Clicked index " + index);
+              this.setState({ activePlace: index });
             }}
           >
             {place.name}
           </button>
         ))}
-        <WeatherDisplay zip={"12345"} />
+        <WeatherDisplay
+         key={activePlace}
+         zip={PLACES[activePlace].zip}
+         />
       </div>
     );
   }
