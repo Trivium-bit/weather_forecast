af0c3ffa40e54959f3d1c0b22a25379be53e5af3
Trivium-bit <trivium.exe@gmail.com>
2021-11-02 15:39:15 +0300

    componentDidMount

--- src/App.js
+++ src/App.js
@@ -9,8 +9,38 @@ const PLACES = [
 ];
 
 class WeatherDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null
+    };
+  }
+  componentDidMount() {
+    const zip = this.props.zip;
+    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+      zip +
+      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    fetch(URL).then(res => res.json()).then(json => {
+      this.setState({ weatherData: json });
+    });
+  }
   render() {
-    return <h1>Displaying weather for city {this.props.zip}</h1>;
+    const weatherData = this.state.weatherData;
+    if (!weatherData) return <div>Loading</div>;
+    const weather = weatherData.weather[0];
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    return (
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData.description} />
+        </h1>
+        <p>Current: {weatherData.main.temp}°</p>
+        <p>High: {weatherData.main.temp_max}°</p>
+        <p>Low: {weatherData.main.temp_min}°</p>
+        <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+      </div>
+    );
   }
 }
 
@@ -18,7 +48,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      activePlace: 0,
+      activePlace: 0
     };
   }
   render() {
@@ -35,10 +65,7 @@ class App extends Component {
             {place.name}
           </button>
         ))}
-        <WeatherDisplay
-         key={activePlace}
-         zip={PLACES[activePlace].zip}
-         />
+        <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
       </div>
     );
   }
